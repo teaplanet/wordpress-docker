5c328ac4063fa4afb6877db8391df3012afc5f5d
Ken <ken+git@teapla.net>
2014-01-03 23:08:08 +0900

    nohup削除。

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-nohup sudo docker run -p 127.0.0.1:10022:22 -p 127.0.0.1:10080:80 -v /data/blog:/blog:rw ken/blog
+sudo docker run -p 127.0.0.1:10022:22 -p 127.0.0.1:10080:80 -v /data/blog:/blog:rw ken/blog
 
 # debug
 #sudo docker run -i -t -p 127.0.0.1:10022:22 -p 127.0.0.1:10080:80 -v /data/blog:/blog:rw ken/blog /bin/bash
